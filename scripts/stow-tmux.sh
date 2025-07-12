0d6da439afdc5d7ae001a0f5c693680eb9ce4e54
Pedro CVLAB <vaz.valois@hotmail.com>
2025-07-12 18:59:55 +0900

    lazyvim updates

--- scripts/stow-tmux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DOTFILES_DIR="$(dirname "$(dirname "$(readlink -f "$0")")")"
-TARGET_DIR="$HOME"
-
-echo "Stowing tmux..."
-stow -t "$TARGET_DIR" -d "$DOTFILES_DIR" tmux
-echo "tmux stowed."
--- scripts/unstow-tmux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DOTFILES_DIR="$(dirname "$(dirname "$(readlink -f "$0")")")"
-TARGET_DIR="$HOME"
-
-echo "Unstowing tmux..."
-stow -D -t "$TARGET_DIR" -d "$DOTFILES_DIR" tmux
-echo "tmux unstowed."
