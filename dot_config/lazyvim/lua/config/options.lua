4e3c7d7dc40843d943867262ec04453d974c4873
Pedro Valois <pedro@cvlab.cs.tsukuba.ac.jp>
2025-08-01 14:27:19 +0900

    pyrefly removed

--- dot_config/lazyvim/lua/config/options.lua
+++ dot_config/lazyvim/lua/config/options.lua
@@ -6,6 +6,6 @@ vim.opt.wrap = true
 
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
-vim.g.lazyvim_python_lsp = "pyrefly"
+-- vim.g.lazyvim_python_lsp = "pyrefly"
 -- Set to "ruff_lsp" to use the old LSP implementation version.
 vim.g.lazyvim_python_ruff = "ruff"
